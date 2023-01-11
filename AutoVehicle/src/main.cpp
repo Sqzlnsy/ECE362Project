ee5a0e0d126ce2d8628c6ed3d670c173ef2123a9
Su <62003760+Sqzlnsy@users.noreply.github.com>
2023-01-10 20:59:05 -0500

    removing stuff

--- AutoVehicle/src/main.cpp
+++ AutoVehicle/src/main.cpp
@@ -9,95 +9,194 @@
 #endif
 
 #include <Arduino.h>
-#include "Wire.h"
 #include "SPI.h"
+#include <BluetoothSerial.h>
+#include <ESP32Servo.h>
+#include "HUSKYLENS.h"
+#include "SoftwareSerial.h"
+#include <Wire.h>
+#include <Adafruit_INA219.h>
 
-// define two tasks for Blink & AnalogRead
-void TaskBlink( void *pvParameters );
-void TaskAnalogReadA3( void *pvParameters );
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
 
-// the setup function runs once when you press reset or power the board
+BluetoothSerial SerialBT;
+// create two servo objects
+Servo backMotor;
+Servo frontMotor;
+
+// These are all GPIO pins on the ESP32
+// Recommended pins include 2,4,12-19,21-23,25-27,32-33
+int backMotorPin = 33;
+int frontMotorPin = 32;
+// Published values for SG90 servos; adjust if needed
+int minUs = 500;
+int maxUs = 2400;
+
+ESP32PWM pwm;
+
+HUSKYLENS huskylens;
+//HUSKYLENS green line >> SDA; blue line >> SCL
+
+Adafruit_INA219 ina219;
+
+void getCurrent();
+
 void setup() {
-  
-  // initialize serial communication at 115200 bits per second:
+  // Allow allocation of all timers
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
   Serial.begin(115200);
-  
-  // Now set up two tasks to run independently.
-  xTaskCreatePinnedToCore(
-    TaskBlink
-    ,  "TaskBlink"   // A name just for humans
-    ,  1024  // This stack size can be checked & adjusted by reading the Stack Highwater
-    ,  NULL
-    ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
-    ,  NULL 
-    ,  ARDUINO_RUNNING_CORE);
+  backMotor.setPeriodHertz(50);      // Standard 50hz servo
+	frontMotor.setPeriodHertz(50);      // Standard 50hz servo
+  backMotor.attach(backMotorPin, minUs, maxUs);
+  frontMotor.attach(frontMotorPin, minUs, maxUs);
 
-  xTaskCreatePinnedToCore(
-    TaskAnalogReadA3
-    ,  "AnalogReadA3"
-    ,  1024  // Stack size
-    ,  NULL
-    ,  1  // Priority
-    ,  NULL 
-    ,  ARDUINO_RUNNING_CORE);
+  SerialBT.begin("Leopard 2 Bluetooth Module");  //Bluetooth device name
+  // Serial.println("The device started, now you can pair it with bluetooth!");
 
-  // Now the task scheduler, which takes over control of scheduling individual tasks, is automatically started.
-}
+  Wire.begin();
 
-void loop()
-{
-  // Empty. Things are done in Tasks.
-}
+  while (!huskylens.begin(Wire)) {
+    String result = String() + F("Begin failed!\n") + F("1.Please recheck the \"Protocol Type\" in HUSKYLENS (General Settings>>Protocol Type>>I2C)\n") + 
+                    F("2.Please recheck the connection.\n");
+    for(int i = 0; i < result.length(); i++) {
+      SerialBT.write(result.charAt(i));
+    }
+    delay(100);
+  }
 
-/*--------------------------------------------------*/
-/*---------------------- Tasks ---------------------*/
-/*--------------------------------------------------*/
+  if (!ina219.begin()) {
+    String result = String() + F("Failed to find INA219 chip");
+    for(int i = 0; i < result.length(); i++) {
+      SerialBT.write(result.charAt(i));
+    }
+    while (1) { delay(10); }
+  }
+  // To use a slightly lower 32V, 1A range (higher precision on amps):
+  //ina219.setCalibration_32V_1A();
+  // Or to use a lower 16V, 400mA range (higher precision on volts and amps):
+  //ina219.setCalibration_16V_400mA();
 
-void TaskBlink(void *pvParameters)  // This is a task.
-{
-  (void) pvParameters;
-
-/*
-  Blink
-  Turns on an LED on for one second, then off for one second, repeatedly.
-    
-  If you want to know what pin the on-board LED is connected to on your ESP32 model, check
-  the Technical Specs of your board.
-*/
-
-  // initialize digital LED_BUILTIN on pin 13 as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-
-  for (;;) // A Task shall never return or exit.
-  {
-    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-    // arduino-esp32 has FreeRTOS configured to have a tick-rate of 1000Hz and portTICK_PERIOD_MS
-    // refers to how many milliseconds the period between each ticks is, ie. 1ms.
-    vTaskDelay(1000 / portTICK_PERIOD_MS );  // vTaskDelay wants ticks, not milliseconds
-    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-    vTaskDelay(1000 / portTICK_PERIOD_MS); // 1 second delay
+  String result = String() + F("Measuring voltage and current with INA219 ...\n");
+  for(int i = 0; i < result.length(); i++) {
+    SerialBT.write(result.charAt(i));
   }
 }
 
-void TaskAnalogReadA3(void *pvParameters)  // This is a task.
-{
-  (void) pvParameters;
-  
-/*
-  AnalogReadSerial
-  Reads an analog input on pin A3, prints the result to the serial monitor.
-  Graphical representation is available using serial plotter (Tools > Serial Plotter menu)
-  Attach the center pin of a potentiometer to pin A3, and the outside pins to +5V and ground.
+void loop() {
+  int xOrigin = 0;
+  int yOrigin = 0;
+  int xTarget = 0;
+  int yTarget = 0;
+  String stringResult = "";
 
-  This example code is in the public domain.
-*/
+  if (!huskylens.request()) Serial.println(F("Fail to request data from HUSKYLENS, recheck the connection!"));
+  else if (!huskylens.isLearned()) Serial.println(F("Nothing learned, press learn button on HUSKYLENS to learn one!"));
+  else if (!huskylens.available()) Serial.println(F("No block or arrow appears on the screen!"));
+  else {
+    Serial.println(F("###########"));
+    if (huskylens.available()) {
+      HUSKYLENSResult result = huskylens.read();
+      if (result.command == COMMAND_RETURN_BLOCK) {
+        Serial.println(String() + F("Block:xCenter=") + result.xCenter + F(",yCenter=") + result.yCenter + F(",width=") + result.width + F(",height=") + result.height + F(",ID=") + result.ID);
+      } else if (result.command == COMMAND_RETURN_ARROW) {
+        xOrigin = result.xOrigin;
+        yOrigin = result.yOrigin;
+        xTarget = result.xTarget;
+        yTarget = result.yTarget;
+        // getHuskyResult(result)
+        stringResult = String() + F("Arrow:xOrigin=") + result.xOrigin + F(",yOrigin=") + result.yOrigin + F(",xTarget=") + result.xTarget + F(",yTarget=") + result.yTarget + F(",ID=") + result.ID;
+        Serial.println(stringResult);
+      } else {
+        Serial.println("Object unknown!");
+      }
+    }
+  }
 
-  for (;;)
-  {
-    // read the input on analog pin A3:
-    int sensorValueA3 = analogRead(A3);
-    // print out the value you read:
-    Serial.println(sensorValueA3);
-    vTaskDelay(100 / portTICK_PERIOD_MS);  // 100ms delay
+  // for (int i = 0; i < stringResult.length(); i++){
+  //   SerialBT.write(stringResult.charAt(i));
+  // }
+  // SerialBT.write('\n');
+
+  double maxYValue = 240.0;
+  double maxSpeed = 120.0;
+  double minSpeed = 50.0;
+  int speed = 0;
+  //Serial.println("yOrigin: " + yOrigin);
+  //Serial.println("yTarget: " + yTarget);
+  frontMotor.write(90);
+  //if ((yOrigin - yTarget) > 0) {
+    speed = (yOrigin-yTarget)/maxYValue*(maxSpeed-minSpeed) + minSpeed; // Max value of 240
+    speed = round((speed+5)/10) * 10; //Rounding to nearest 10's place (THIS SHOULD WORK BUT DOUBLE CHECK)
+    //Serial.println("Speed: " + speed);
+    for(int pos = 60; pos < 120; pos += 10) { // Goes from 50 to speed ideally
+      backMotor.write(pos);
+      delay(200);
+      Serial.println(pos);
+    }
+  //}
+  // if ((xOrigin - xTarget) > 20) {
+  //   //Go left //Max value of 320
+  // } else if ((xOrigin - xTarget) < 20) {
+  //   //Go right //Min value of -320
+  // } else {
+  //   frontMotor.write(90);
+  // }
+  getCurrent();
+
+  delay(300);
+}
+
+void getCurrent() {
+  float shuntvoltage = 0;
+  float busvoltage = 0;
+  float current_mA = 0;
+  float loadvoltage = 0;
+  float power_mW = 0;
+
+  shuntvoltage = ina219.getShuntVoltage_mV();
+  busvoltage = ina219.getBusVoltage_V();
+  current_mA = ina219.getCurrent_mA();
+  power_mW = ina219.getPower_mW();
+  loadvoltage = busvoltage + (shuntvoltage / 1000);
+
+  String result = String() + F("Bus Voltage: ") + busvoltage + F(" V\n") + 
+                  F("Shunt Voltage: ") + shuntvoltage + F(" mV\n") +
+                  F("Load Voltage: ") + loadvoltage + F(" V\n") + 
+                  F("Current: ") + current_mA + F(" mA\n") +
+                  F("Power: ") + power_mW + F(" mW\n");
+
+  for(int i = 0; i < result.length(); i++) {
+    SerialBT.write(result.charAt(i));
   }
 }
+
+// void loop() {
+//   for (pos = 50; pos < 120; pos += 10) {  // sweep from 0 degrees to 180 degrees
+//     // in steps of 1 degree
+//     backMotor.write(pos);
+//     delay(100);  // waits 20ms for the servo to reach the position
+//   }
+//   delay(2000);
+//   for (pos = 120; pos > 50; pos -= 10) {  // sweep from 180 degrees to 0 degrees
+//     backMotor.write(pos);
+//     delay(100);
+//   }
+//   backMotor.write(0);
+//   delay(2000);
+//   for (pos = 20; pos < 160; pos += 10) {  // sweep from 0 degrees to 180 degrees
+//     // in steps of 1 degree
+//     frontMotor.write(pos);
+//     delay(100);  // waits 20ms for the servo to reach the position
+//   }
+//   for (pos = 160; pos > 20; pos -= 10) {  // sweep from 180 degrees to 0 degrees
+//     frontMotor.write(pos);
+//     delay(100);
+//   }
+//   backMotor.detach();
+//   frontMotor.detach();
+
+//   delay(5000);
+// }
